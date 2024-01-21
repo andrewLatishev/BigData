8b726fdd500452b96f6898cee6470203c02adc20
andrewLatishev <83699418+andrewLatishev@users.noreply.github.com>
2024-01-21 22:00:07 +0300

    Update BigDataSQL.sql

--- BigDataSQL.sql
+++ BigDataSQL.sql
@@ -12,3 +12,18 @@ count(case when assessment = 5 then 1 end) as "кол-во пятерок"
 from assessment 
 where data between '2023-01-01' and '2023-01-31'
 group by fio
+
+
+"Дана таблица:
+ students (id INTEGER PRIMARY KEY, first_name TEXT NOT NULL,
+ last_name TEXT NOT NULL, email TEXT NOT NULL UNIQUE,
+ phone TEXT NOT NULL UNIQUE, salary INTEGER NOT NULL);
+Нужно найти всех студентов, которые среди своих однофамильцев имеют максимальную стипендию. Итоговая выборка должна содержать id, first_name, last_name и salary студента.
+"
+select id, first_name, last_name, salary
+from students
+where (last_name, salary) in (
+	select last_name, max(salary)
+	from students
+	group by last_name
+)
